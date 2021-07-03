4979ec29d74c5def39564654e8c20de21590c77d
rsh249 <rsh249@cornell.edu>
2021-07-03 16:40:12 -0400

    update visualization with basic tileplot exploration

--- R/neotoma_metabarcoding.R
+++ R/neotoma_metabarcoding.R
@@ -1,6 +1,10 @@
 #### R Script to analyze DNA metabarcode data obtained from ancient packrat middens
 #load libraries
 library(stringr)
+library(dplyr)
+library(ggplot2)
+library(RColorBrewer)
+pal = brewer.pal(name='Paired', n = 12)
 
 # get data from zenodo repository and unzip
 download.file("https://zenodo.org/record/5068129/files/SURE2021.tar.gz?download=1", 'SURE2021.tar.gz')
@@ -50,5 +54,45 @@ for(d in data){
 }
 
 # visualize results
+krep.files = list.files('results', full.names = T, recursive = T, pattern='kreport')
+l <- lapply(krep.files, data.table::fread)
+for(i in 1:length(l)){
+  filename = krep.files[[i]]
+  #samname = tools::file_path_sans_ext(basename(filename)) 
+  samname = paste(word(filename, start=-2, sep='/'), word(filename, start=-1, sep='/'), sep='_')
+  samname = word(samname, start = 1, sep = '[.]')
+  l[[i]]$samname = samname
+}
 
+krep <- bind_rows(l)
+
+kproc.sample = krep %>%
+  filter(V4=='F') %>% 
+  filter(V1>=0.05) %>%
+  group_by(samname, V6) %>%
+  summarise(readcount = sum(V2)) %>%
+  mutate(freq = readcount / sum(readcount)) %>% 
+  filter(freq>0.05)
+
+
+
+ktileplot_scaled = ggplot(data = kproc.sample ) +
+  geom_tile(aes(x=samname, y = V6, fill = readcount)) +
+  theme_linedraw() +
+  theme(legend.position = 'right',
+        axis.text.y = element_text(angle=45),
+        axis.text.x = element_text(angle=45, vjust =1, hjust = 1)) +
+  scale_fill_steps(low=pal[1], high=pal[2], n.breaks = 10) +
+  xlab('') + ylab('')
+ktileplot_scaled
+
+ktileplot = ggplot(data = krep %>% filter(V4=='G') %>% filter(V2>5) ) +
+  geom_tile(aes(x=samname, y = V6, fill = V2)) +
+  theme_linedraw() +
+  theme(legend.position = 'right',
+        axis.text.y = element_text(angle=45),
+        axis.text.x = element_text(angle=45, vjust =1, hjust = 1)) +
+  scale_fill_steps(low=pal[1], high=pal[2], n.breaks = 10) +
+  xlab('') + ylab('')
+ktileplot
 
